6712ff0afb23675d5a09a0f445f5bea00758fb5c
Yousuf007 <yousufjobaer@gmail.com>
2024-09-19 20:58:52 +0600

    fix the page issue

--- js/Login.js
+++ js/Login.js
@@ -5,7 +5,7 @@ document.getElementById('login-btn').addEventListener('click', function(event){
 
     if(inputNumber === '5' && inputPin === '1234'){
         console.log('apni shothik input diyechen');
-        window.location.href = '/home.html';
+        window.location.href = './home.html';
     }
     else{
         alert('Please give us valid input or pin number');
